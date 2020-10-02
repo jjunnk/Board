6e8b4c5853670095da13be21c1e0e974a8bc7efe
jjunnk <artisj1111@gmail.com>
2020-10-02 18:25:26 +0900

    10.02 -2

--- login/src/error.js
+++ login/src/error.js
@@ -1,14 +1,8 @@
 import Vue from 'vue'
 
 Vue.config.errorHandler = e => {
-<<<<<<< HEAD
     console.log(e.message)
     console.log('here')
 
 
-=======
-  console.error(e.message)
-  Vue.prototype.$toast.error(e.message)
-  console.log('here')
->>>>>>> 076f3b43180ae228af9c809080ef79118d68b036
 }
