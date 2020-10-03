3f967817f207805e12ab7b227d93a0de40cdbad4
jjunnk <artisj1111@gmail.com>
2020-10-03 23:05:24 +0900

    realtime 읽고 쓰기

--- vb/src/App.vue
+++ vb/src/App.vue
@@ -5,11 +5,14 @@
             <v-app-bar-nav-icon @click="drawer =!drawer"></v-app-bar-nav-icon>
             <site-title v-bind:propstitle="title"></site-title>
             <v-spacer />
-            <v-btn icon to="/about">
+            <v-btn icon @click="save">
+                <v-icon>mdi-check</v-icon>
+            </v-btn>
+            <v-btn icon @click="read">
                 <v-icon>mdi-circle</v-icon>
             </v-btn>
-            <v-btn icon to="/">
-                <v-icon>mdi-home</v-icon>
+            <v-btn icon @click="readOne">
+                <v-icon>mdi-heart</v-icon>
             </v-btn>
         </v-app-bar>
         <v-navigation-drawer v-model="drawer" app>
@@ -44,6 +47,28 @@ export default {
             group: null,
             title: '사이트 제목',
             footer: 'sujin'
+        }
+    },
+    mounted() {
+        console.log(this.$firebase)
+    },
+    methods: {
+        save() {
+            console.log('save click')
+            this.$firebase.database().ref().child('abcd').set({
+                title: 'abcd',
+                text: 'tttttexttttt'
+            })
+        },
+        read() {
+            this.$firebase.database().ref().child('abcd').on('value', (sn) => {
+                console.log(sn)
+                console.log(sn.val())
+            })
+        },
+        async readOne() {
+            const sn = await this.$firebase.database().ref().child('abcd').once('value')
+            console.log(sn.val())
         }
     }
 }
