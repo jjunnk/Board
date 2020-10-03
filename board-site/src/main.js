17682d359dd6c814ed70e4633eaf440e54e1b8a9
jjunnk <artisj1111@gmail.com>
2020-10-03 12:12:11 +0900

    10.03 base

--- board-site/src/main.js
+++ board-site/src/main.js
@@ -1,9 +1,15 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 
-  new Vue({
-    render: h => h(App),
-}).$mount('#app');
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
--- board-site/src/store/index.js
+++ board-site/src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore } from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
 export default createStore({
   state: {},
