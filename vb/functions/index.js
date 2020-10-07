9dd7942d80b582eed2ee25e46eaa954acdb17b85
jjunnk <artisj1111@gmail.com>
2020-10-07 19:22:12 +0900

    10.07 24 functions로 사용자 저장하기

--- vb/functions/index.js
+++ vb/functions/index.js
@@ -1,9 +1,34 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+var admin = require('firebase-admin')
+
+var serviceAccount = require('./key.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://x-sujin.firebaseio.com'
+})
+
+const db = admin.database()
+
+// 계정이 들어올때  / exports.createUser 라는 function 생성
+exports.createUser = functions.auth.user().onCreate(async (user) => {
+  const { uid, email, displayName, photoURL } = user
+  const u = {
+    email,
+    displayName,
+    photoURL,
+    createdAt: new Date()
+  }
+  db.ref('users').child(uid).set(u) // uid 키값 , u 데이터
+})
+
+// 계정이 지워질때
+exports.deleteUser = functions.auth.user().onDelete(async (user) => {
+  const {
+    uid
+  } = user
+  db.ref('users').child(uid).remove() // uid 키값 , u 데이터
+
+  // ...
+})
