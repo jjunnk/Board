2c7d741e94c3b2cacc4f39927c2ca92d38893902
jjunnk <artisj1111@gmail.com>
2020-10-14 18:48:40 +0900

    33. fire storage 맛보기 & menu 추가 코드 수정

--- vb/src/App.vue
+++ vb/src/App.vue
@@ -1,20 +1,20 @@
 <template>
 <v-app>
-    <v-card class="overflow-hidden">
-        <v-app-bar absolute app color="white">
-            <v-app-bar-nav-icon @click="drawer =!drawer"></v-app-bar-nav-icon>
-            <site-title v-bind:propsTitle="site.title"></site-title>
-            <v-spacer />
-            <site-sign></site-sign>
-        </v-app-bar>
-        <v-navigation-drawer v-model="drawer" app>
-            <site-menu :items="site.menu"></site-menu>
-        </v-navigation-drawer>
-        <v-main>
-            <router-view />
-        </v-main>
-        <site-footer :propsFooter="site.footer"></site-footer>
-    </v-card>
+  <v-card class="overflow-hidden">
+    <v-app-bar absolute app color="white">
+      <v-app-bar-nav-icon @click="drawer =!drawer"></v-app-bar-nav-icon>
+      <site-title v-bind:propsTitle="site.title"></site-title>
+      <v-spacer />
+      <site-sign></site-sign>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer" :width="380">
+      <site-menu :items="site.menu"></site-menu>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view />
+    </v-main>
+    <site-footer :propsFooter="site.footer"></site-footer>
+  </v-card>
 </v-app>
 </template>
 
@@ -25,63 +25,63 @@ import SiteMenu from '@/views/site/menu'
 import SiteSign from '@/views/site/sign'
 
 export default {
-    name: 'App',
+  name: 'App',
 
-    components: {
-        SiteTitle,
-        SiteFooter,
-        SiteMenu,
-        SiteSign
-    },
+  components: {
+    SiteTitle,
+    SiteFooter,
+    SiteMenu,
+    SiteSign
+  },
 
-    data() {
-        return {
-            drawer: false,
-            site: {
-                menu: [{
-                        title: 'home',
-                        icon: 'mdi-home',
-                        subItems: [{
-                                title: 'Dashboard',
-                                to: '/'
-                            },
-                            {
-                                title: 'About',
-                                to: '/about'
-                            }
-                        ]
-                    },
-                    {
-                        title: 'about',
-                        active: true,
-                        icon: 'mdi-account',
-                        subItems: [{
-                            title: 'xxx',
-                            to: '/xxx'
-                        }]
-                    }
-                ],
-                title: '사이트 제목',
-                footer: 'sujin'
-            }
-        }
-    },
-    created() {
-        this.subscribe()
-    },
-    methods: {
-        subscribe() {
-            this.$firebase.database().ref().child('site').on('value', (sn) => {
-                const v = sn.val()
-                if (!v) {
-                    this.$firebase.database().ref().child('site').set(this.site)
-                    return
-                }
-                this.site = v
-            }, (e) => {
-                console.log(e.message)
-            })
-        }
+  data() {
+    return {
+      drawer: false,
+      site: {
+        menu: [{
+            title: 'home',
+            icon: 'mdi-home',
+            subItems: [{
+                title: 'Dashboard',
+                to: '/'
+              },
+              {
+                title: 'About',
+                to: '/about'
+              }
+            ]
+          },
+          {
+            title: 'about',
+            active: true,
+            icon: 'mdi-account',
+            subItems: [{
+              title: 'xxx',
+              to: '/xxx'
+            }]
+          }
+        ],
+        title: '사이트 제목',
+        footer: 'sujin'
+      }
     }
+  },
+  created() {
+    this.subscribe()
+  },
+  methods: {
+    subscribe() {
+      this.$firebase.database().ref().child('site').on('value', (sn) => {
+        const v = sn.val()
+        if (!v) {
+          this.$firebase.database().ref().child('site').set(this.site)
+          return
+        }
+        this.site = v
+      }, (e) => {
+        console.log(e.message)
+      })
+    }
+  }
 }
 </script>
---
+++ vb/src/views/storage.vue
@@ -0,0 +1,23 @@
+<template>
+<v-sheet>
+  Storage
+  <v-btn @click="write" outlined> write</v-btn>
+</v-sheet>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    async write() {
+      const sn = await this.$firebase.storage().ref().child('xxx.text').putString('ABCD')
+      const url = await sn.ref.getDownloadURL()
+      console.log(url)
+    }
+  }
+}
+</script>
