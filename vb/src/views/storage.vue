44fa07cdfb269613726474e4f542a861b9b3788f
jjunnk <artisj1111@gmail.com>
2020-10-15 00:05:29 +0900

    34. axios로 storage 데이터 가져오기

--- vb/src/views/storage.vue
+++ vb/src/views/storage.vue
@@ -1,15 +1,19 @@
 <template>
 <v-sheet>
-  Storage
+  <v-textarea v-model="text">
+  </v-textarea>
   <v-btn @click="write" outlined> write</v-btn>
+  <v-btn @click="read" outlined> read</v-btn>
 </v-sheet>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
-      text: ''
+      text: '',
+      utl: ''
     }
   },
   methods: {
@@ -17,6 +21,12 @@ export default {
       const sn = await this.$firebase.storage().ref().child('xxx.text').putString('ABCD')
       const url = await sn.ref.getDownloadURL()
       console.log(url)
+      this.url = url
+    },
+    async read() {
+      const r = await axios.get(this.url)
+      console.log(r)
+      this.text = r.data
     }
   }
 }
