0f6b0e4ff1a8572ce852063843d15b94b4ac83a1
jjunnk <artisj1111@gmail.com>
2020-11-01 13:28:56 +0900

    48 글 삭제하기 article 개수 업데이트, 게시물 삭제, 게시글 삭제

--- vuesite/src/components/display-content.vue
+++ vuesite/src/components/display-content.vue
@@ -9,6 +9,9 @@
         <v-btn icon @click="articleWrite">
             <v-icon>mdi-pencil</v-icon>
         </v-btn>
+        <v-btn icon @click="remove">
+            <v-icon>mdi-delete</v-icon>
+        </v-btn>
         <v-btn icon @click="$emit('close')">
             <v-icon>mdi-close</v-icon>
         </v-btn>
@@ -48,7 +51,8 @@ export default {
     },
     data() {
         return {
-            content: ''
+            content: '',
+            ref: this.$firebase.firestore().collection('boards').doc(this.document)
         }
     },
     mounted() {
@@ -59,7 +63,7 @@ export default {
         async fetch() {
             const r = await axios.get(this.item.url)
             this.content = r.data
-            await this.$firebase.firestore().collection('boards').doc(this.document).collection(this.item.id).update({
+            await this.ref.collection('articles').doc(this.document).collection(this.item.id).update({
                 readCount: this.$firebase.firestore.FieldValue.increment(1)
             })
         },
@@ -70,6 +74,19 @@ export default {
                     articleId: this.item.id
                 }
             })
+        },
+        async remove() {
+            const batch = this.$firebase.firestore().batch()
+            batch.update(this.ref, {
+                count: this.$firebase.firestore.FieldValue.increment(-1)
+            })
+            batch.delete(this.ref.collection('articles').doc(this.item.id).delete())
+            await batch.commit()
+            // 아티클 게시물 삭제하려면 count, 게시물, storage 파일 => 3 개를 삭제해야함
+            //await this.ref.update({ count: this.$firebase.firestore.FieldValue.increment(-1)})  // 조회수
+            // await this.ref.collection('articles').doc(this.item.id).delete() // 게시물 삭제
+            await this.$firebase.storage().ref().child('boards').child(this.document).child(this.item.id + '.md').delete() // storage 파일 삭제
+            this.$emit('close')
         }
 
     }
