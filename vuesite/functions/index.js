a1c33a092f2dd842d8a3820f661d5133fdc12d34
jjunnk <artisj1111@gmail.com>
2020-11-02 15:22:07 +0900

    54 firebase 규칙 개선하기

--- vuesite/functions/index.js
+++ vuesite/functions/index.js
@@ -19,7 +19,9 @@ exports.createUser = functions.auth.user().onCreate(async (user) => {
     displayName,
     photoURL,
     createdAt: time,
-    level: email === functions.config().admin.email ? 0 : 5
+    level: email === functions.config().admin.email ? 0 : 5,
+    visitedAt : time,
+    visitCount: 0
   }
   await fdb.collection('users').doc(uid).set(u)
   u.createdAt = time.getTime()
