4b81acc7931353e1a33d784171093e21440fd106
jjunnk <artisj1111@gmail.com>
2020-11-02 15:22:25 +0900

    54 firebase 규칙 개선

--- vuesite/src/App.vue
+++ vuesite/src/App.vue
@@ -1,22 +1,23 @@
 <template>
-  <v-app>
+<v-app>
     <v-card class="overflow-hidden" height="100%">
-      <v-app-bar color="white">
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-        <SiteTitle :propstitle="site.title"></SiteTitle>
-        <v-spacer />
-        <SiteSign></SiteSign>
-      </v-app-bar>
-      <v-navigation-drawer app v-model="drawer" width="400">
-        <SiteMenu :items="site.menu" @close="drawer = false"></SiteMenu>
-      </v-navigation-drawer>
-      <v-main>
-        <router-view />
-      </v-main>
+        <v-app-bar color="white">
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <SiteTitle :propstitle="site.title"></SiteTitle>
+            <v-spacer />
+            <SiteSign></SiteSign>
+            <v-btn @click="test">test</v-btn>
+        </v-app-bar>
+        <v-navigation-drawer app v-model="drawer" width="400">
+            <SiteMenu :items="site.menu" @close="drawer = false"></SiteMenu>
+        </v-navigation-drawer>
+        <v-main>
+            <router-view />
+        </v-main>
 
-      <SiteFooter :footer="site.footer"></SiteFooter>
+        <SiteFooter :footer="site.footer"></SiteFooter>
     </v-card>
-  </v-app>
+</v-app>
 </template>
 
 <script>
@@ -26,78 +27,79 @@ import SiteMenu from "@/views/site/menu.vue";
 import SiteSign from "@/views/site/sign.vue";
 
 export default {
-  name: "App",
-  components: {
-    SiteTitle,
-    SiteFooter,
-    SiteMenu,
-    SiteSign
-  },
-  data() {
-    return {
-      drawer: false,
-      site: {
-        title: "Vue Site",
-        footer: "Footer text",
-        menu: [
-          {
-            title: "Home",
-            icon: "mdi-home",
-            subItems: [
-              {
-                title: "Dashboard",
-                to: "/"
-              }
-            ]
-          },
-          {
-            title: "About",
-            icon: "mdi-heart",
-            active: true,
-            subItems: [
-              {
-                title: "Dashboard",
-                to: "/about"
-              }
-            ]
-          },
-          {
-            title: "xxx",
-            icon: "mdi-circle",
-            to: "/xxx"
-          }
-        ]
-      }
-    };
-  },
-  created() {
-    this.subscribe();
-  },
-  methods: {
-    subscribe() {
-      this.$firebase
-        .database()
-        .ref()
-        .child("site")
-        .on(
-          "value",
-          sn => {
-            const v = sn.val();
-            if (!v) {
-              this.$firebase
+    name: "App",
+    components: {
+        SiteTitle,
+        SiteFooter,
+        SiteMenu,
+        SiteSign
+    },
+    data() {
+        return {
+            drawer: false,
+            site: {
+                title: "Vue Site",
+                footer: "Footer text",
+                menu: [{
+                        title: "Home",
+                        icon: "mdi-home",
+                        subItems: [{
+                            title: "Dashboard",
+                            to: "/"
+                        }]
+                    },
+                    {
+                        title: "About",
+                        icon: "mdi-heart",
+                        active: true,
+                        subItems: [{
+                            title: "Dashboard",
+                            to: "/about"
+                        }]
+                    },
+                    {
+                        title: "xxx",
+                        icon: "mdi-circle",
+                        to: "/xxx"
+                    }
+                ]
+            }
+        };
+    },
+    created() {
+        this.subscribe();
+    },
+    methods: {
+        subscribe() {
+            this.$firebase
                 .database()
                 .ref()
                 .child("site")
-                .set(this.site);
-            }
-            this.site = v;
-          },
-          e => {
-            console.log(e.message);
-          }
-        );
+                .on(
+                    "value",
+                    sn => {
+                        const v = sn.val();
+                        if (!v) {
+                            this.$firebase
+                                .database()
+                                .ref()
+                                .child("site")
+                                .set(this.site);
+                        }
+                        this.site = v;
+                    },
+                    e => {
+                        console.log(e.message);
+                    }
+                );
+        },
+        async test() {
+            await this.$firebase.firestore().collection('users').doc(this.$store.state.fireUser.uid).update({
+                visitedAt: new Date(),
+                visitCount: 0
+            })
+        }
     }
-  }
 };
 </script>
 
