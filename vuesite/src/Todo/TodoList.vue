1fd5f372fb3173e40a028e312eff163f2c4fb478
jjunnk <artisj1111@gmail.com>
2020-11-11 14:11:57 +0900

    Add todo project file

---
+++ vuesite/src/Todo/TodoList.vue
@@ -0,0 +1,90 @@
+<template>
+<section>
+    <transition-group name="list" tag="ul">
+        <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow">
+            <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+            {{ todoItem }}
+            <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                <i class="far fa-trash-alt" aria-hidden="true"></i>
+            </span>
+        </li>
+    </transition-group>
+</section>
+</template>
+
+<script>
+export default {
+    props: ["propsdata"],
+    /*
+
+  //불필요하여 삭제
+    data() {
+    return {
+        todoItems: [],
+    };
+},
+
+  //App.vue 파일로 이동
+    created() {
+        if (localStorage.length > 0) {
+            for (var i = 0; i < localStorage.length; i++) {
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    },*/
+    methods: {
+        removeTodo(todoItem, index) {
+            this.$emit("removeAll");
+            /*
+                        // App.vue - removeTodo methods 로 이동
+                        localStorage.removeItem(todoItem);
+                        this.todoItems.splice(index, 1);
+                        */
+            //console.log(todoItem, index);
+        },
+    },
+};
+</script>
+
+<style scoped>
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
+
+li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+
+.checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+    height: 100%;
+}
+
+.removeBtn {
+    margin-left: auto;
+    color: #de4343;
+}
+
+.list-enter-active,
+.lis-leave-active {
+    transition: all 1s;
+}
+
+.list-enter,
+.list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+}
+</style>
