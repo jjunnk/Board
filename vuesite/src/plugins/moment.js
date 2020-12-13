7c60abf91cb2dc7925922aac6c3cebc6bf35e1cc
jjunnk <artisj1111@gmail.com>
2020-12-13 22:13:14 +0900

    portfolio folder

--- vuesite/src/plugins/moment.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import moment from 'moment'
-import 'moment/locale/ko'
-
-moment.locale('ko')
-
-Vue.prototype.$moment = moment
--- vuesite/src/plugins/scratchable.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import VueScratchable from 'vue-scratchable';
-
-Vue.component('vue-scratchable', VueScratchable);
