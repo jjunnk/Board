a9f754437cf8722bc8c23f7bfd29169c6f5031c0
jjunnk <70044280+jjunnk@users.noreply.github.com>
2020-12-26 23:55:25 +0900

    1

--- source/vue.config.js
+++ source/vue.config.js
@@ -3,6 +3,15 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     transpileDependencies: ["vuetify"],
     publicPath: '/',
+    pwa: {
+      iconPaths: {
+        favicon32: "./public/favicon.png",
+        favicon16: "./public/favicon.png",
+        appleTouchIcon: "./public/favicon.png",
+        maskIcon: "./public/favicon.png",
+        msTileImage: "./public/favicon.png"
+      }
+    },
     configureWebpack: {
       plugins:[
         new MiniCssExtractPlugin({
